29f339c600c924914399a022b549772f4086eeed
Jasmin Memisevic <jasmin.memisevic@gmail.com>
2019-05-06 01:45:14 +0200

    basic styles and features for input component

--- src/components/ui_elements/input/Input.js
+++ src/components/ui_elements/input/Input.js
@@ -1,9 +1,85 @@
 import React from 'react';
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+// helpers
+import { getPairKeyValue } from '../../../shared/helpers';
 // styles
 import './input.styles.scss';
 
+/*
+    @props
+    round: bool
+    onBlur: func
+    onFocus: func
+    onChange: func
+    noAnimation: bool
+    size: 'small' || 'large'
+    shadow: 'inset' || 'outset' || 'both'
+*/
+
 export default class Input extends React.Component {
+    static propTypes = {
+        round: PropTypes.bool,
+        onBlur: PropTypes.func,
+        onFocus: PropTypes.func,
+        onChange: PropTypes.func,
+        noAnimation: PropTypes.bool,
+        size: PropTypes.oneOf(['small', 'large']),
+        shadow: PropTypes.oneOf(['inset', 'outset', 'both']),
+    };
+
+    static defaultProps = {
+        size: null,
+        shadow: null,
+        onBlur: null,
+        round: false,
+        onFocus: null,
+        onChange: null,
+        noAnimation: false,
+    };
+
+    cssClasses = () => {
+        const { size, round, shadow, noAnimation } = this.props;
+
+        return cx(
+            size,
+            'input',
+            shadow && `shadow ${shadow}`,
+            getPairKeyValue(round, 'round'),
+            getPairKeyValue(noAnimation, 'noAnimation')
+        );
+    };
+
+    getHTMLattrs = () => {
+        const cleanHTMLattrs = { ...this.props };
+        const customProps = [
+            'size',
+            'round',
+            'onBlur',
+            'shadow',
+            'onFocus',
+            'onChange',
+            'noAnimation',
+        ];
+
+        customProps.forEach(customProp => {
+            if (cleanHTMLattrs.hasOwnProperty(customProp)) delete cleanHTMLattrs[customProp];
+        });
+
+        return cleanHTMLattrs;
+    };
+
     render() {
-        return <input placeholder="placeholder" type="text" />;
+        const { onChange, onFocus, onBlur } = this.props;
+
+        return (
+            <input
+                onBlur={onBlur}
+                onFocus={onFocus}
+                onChange={onChange}
+                {...this.getHTMLattrs()}
+                className={this.cssClasses()}
+            />
+        );
     }
 }
