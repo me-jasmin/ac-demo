a63e07268399ff7b5193b8f61300a674e64771b8
Jasmin Memisevic <jasmin.memisevic@gmail.com>
2019-05-06 01:44:17 +0200

    helpers updated the the console styles

--- src/shared/helpers.js
+++ src/shared/helpers.js
@@ -1,2 +1,4 @@
 const getPairKeyValue = (val, key) => val && key;
-export { getPairKeyValue };
+const consoleCss = 'font-size: 16px; background-color: #139879; color: #fff; padding: 10px 30px;';
+
+export { getPairKeyValue, consoleCss };
