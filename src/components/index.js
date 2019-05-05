59382938a202751251490563839d08d672c30e1a
Jasmin Memisevic <jasmin.memisevic@gmail.com>
2019-05-05 21:51:51 +0200

    imput component boilerplate added

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import Input from './ui_elements/input';
+import Button from './ui_elements/button';
+
+export { Input, Button };
---
+++ src/components/ui_elements/input/Input.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// styles
+import './input.styles.scss';
+
+export default class Input extends React.Component {
+    render() {
+        return <input placeholder="placeholder" type="text" />;
+    }
+}
