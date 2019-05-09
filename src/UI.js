367edba3081a77896987087a4bcdeebb4b3cec50
Jasmin Memisevic <jasmin.memisevic@gmail.com>
2019-05-09 22:54:01 +0200

    all props are tied with state so they can be updated with the demo controls

--- src/UI.js
+++ src/UI.js
@@ -9,51 +9,94 @@ import 'camp-css/scss/camp.scss';
 import icon from './shared/images/icon.svg';
 
 export default class UI extends React.Component {
+    state = {
+        button: {
+            icon: icon,
+            round: true,
+            primary: true,
+            size: 'normal',
+            text: 'Button',
+            noHover: false,
+            secondary: false,
+            shadow: 'outset',
+            colorVariant: 100,
+            noAnimation: false,
+        },
+        input: {
+            size: null,
+            shadow: null,
+            round: false,
+            noAnimation: false,
+            placeholder: 'placeholder',
+        },
+    };
+
     // test function to handle button click
-    testButtonClick = event => {
+    handleButton = event => {
         // prevent default event
         event.preventDefault();
+        // element
+        const el = event.target;
         // logging :)
-        console.log('%c Button clicked!  %s', consoleCss, event.target);
+        console.log('%c Button clicked!  %s', consoleCss('#139879'), el);
+    };
+
+    // test function to handle input
+    handleInput = event => {
+        // prevent default event
+        event.preventDefault();
+        // element
+        const el = event.target;
+        // logging :)
+        switch (event.type) {
+            case 'focus':
+                console.log('%c Input focus!  %s', consoleCss('#5884ea'), el);
+                break;
+            case 'blur':
+                console.log('%c Input blur!  %s', consoleCss('#5884ea'), el);
+                break;
+            case 'change':
+                console.log(`%c Input change! value: ${el.value}  %s`, consoleCss('#5884ea'), el);
+                break;
+            default:
+                break;
+        }
     };
 
     render() {
+        const { button, input } = this.state;
+
         return (
             <>
                 <h1>AC UI demo</h1>
-                <Button
-                    shadow="outset"
-                    size="small"
-                    round
-                    colorVariant={100}
-                    onClick={this.testButtonClick}
-                    icon={icon}
-                >
-                    Basic button
-                </Button>
-                <Input placeholder="placeholder" shadow="outset" size="small" round />
-                <Button
-                    secondary
-                    colorVariant={80}
-                    shadow="outset"
-                    round
-                    onClick={this.testButtonClick}
-                    icon={icon}
-                >
-                    Basic button
-                </Button>
-                <Input placeholder="placeholder" shadow="outset" round />
-                <Button
-                    primary
-                    colorVariant={80}
-                    shadow="outset"
-                    size="large"
-                    round
-                    onClick={this.testButtonClick}
-                >
-                    Basic button
-                </Button>
-                <Input placeholder="placeholder" shadow="outset" size="large" round />
+                <div style={{ padding: '40px' }}>
+                    <Input
+                        size={button.size}
+                        round={button.round}
+                        shadow={button.shadow}
+                        onBlur={this.handleInput}
+                        onFocus={this.handleInput}
+                        onChange={this.handleInput}
+                        placeholder={input.placeholder}
+                        noAnimation={button.noAnimation}
+                    />
+                </div>
+                <div style={{ padding: '40px' }}>
+                    <Button
+                        size={button.size}
+                        icon={button.icon}
+                        round={button.round}
+                        shadow={button.shadow}
+                        primary={button.primary}
+                        noHover={button.noHover}
+                        onClick={this.handleButton}
+                        secondary={button.secondary}
+                        noAnimation={button.noAnimation}
+                        colorVariant={button.colorVariant}
+                    >
+                        {button.text}
+                    </Button>
+                </div>
             </>
         );
     }
