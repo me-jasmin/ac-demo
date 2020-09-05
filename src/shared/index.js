0c4c09165ee4b40e2ce908f084344466e5e3c9f8
Jasmin Memisevic <jasmin.memisevic@gmail.com>
2020-09-05 06:59:39 +0200

    exports fixed

--- src/shared/index.js
+++ src/shared/index.js
@@ -1,2 +1,13 @@
-import Input from './Input';
-export default Input;
+import { handleButtonLogging, handleInputLogging } from './eventLogging';
+import { getPairKeyValue, consoleCss, getHTMLattrs } from './helpers';
+import { customProps, elementProps } from './props';
+
+export {
+    handleButtonLogging,
+    handleInputLogging,
+    getPairKeyValue,
+    consoleCss,
+    getHTMLattrs,
+    customProps,
+    elementProps,
+};
